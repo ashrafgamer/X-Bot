1f6d8c930212e5f7229eda112d76f7f40cc02a82
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-14 21:01:40 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = ("+")
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+client.login('token');
